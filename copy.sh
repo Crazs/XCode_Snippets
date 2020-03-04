1cbea9161193cd74925971fddd55dd35f41049dc
Crazs <zhouwentao@zhouwtdeMac-mini.local>
2020-03-04 15:15:43 +0800

    添加代码片段
    添加代码片段

--- copy.sh
+++ copy.sh
@@ -2,14 +2,18 @@
 
 #路径配置
 cur_path=$(cd `dirname $0`;pwd)
-work_path="~/library/developer/xcode/userdata"
+cur_backup=$cur_path"/backup"
+work_path=${HOME}"/library/developer/xcode/userdata"
 work_name='/CodeSnippets'
 curdate="`date +%Y_%m_%d_%H_%M_%S`";
 
 #备份本地snippets
 if [ -d $work_path$work_name ];then
 echo "备份本地代码片段"
-cp -r $work_path$work_name $cur_path$work_name'_'$curdate
+if [ ! -d $cur_backup ];then
+mkdir $cur_backup
+fi
+cp -r $work_path$work_name $cur_backup$work_name'_'$curdate
 else
 echo "创建本地代码片段"
 mkdir $work_path$work_name
