e727ab6153c57ecbf125d86023058593d04e3d0b
Crazs <zhouwentao@zhouwtdeMac-mini.local>
2020-03-07 00:12:09 +0800

    移除不部分方法，减少意外重写

--- Templates/UIViewControllerObjective-C/___FILEBASENAME___.m
+++ Templates/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -10,10 +10,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    //[self createSubView];
-    //[self createNavigationBar];
 }
+//- (void)createSubView{
+//    [super createSubView];
+//}
 
 #pragma mark - HTTP
 
@@ -23,32 +23,9 @@
 
 #pragma mark - Private
 
-//- (void)createNavigationBar{
-//
-//}
-//
-//- (void)createSubView{
-//
-//    [self makeSubViewsLayout];
-//}
-//
-//- (void)makeSubViewsLayout{
-//
-//}
-
 #pragma mark - Setter
 
 #pragma mark - Getter
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Templates/UIViewObjective-C/___FILEBASENAME___.m
+++ Templates/UIViewObjective-C/___FILEBASENAME___.m
@@ -4,37 +4,10 @@
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
-- (instancetype)init{
-    self = [super init];
-    if (self) {
-        [self createSubView];
-    }
-    return self;
-}
-
-- (instancetype)initWithFrame:(CGRect)frame{
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self createSubView];
-    }
-    return self;
-}
-
-- (instancetype)initWithCoder:(NSCoder *)coder{
-    self = [super initWithCoder:coder];
-    if (self) {
-        [self createSubView];
-    }
-    return self;
-}
-
 - (void)createSubView{
+    [super createSubView];
     //初始化子视图
-    [self makeSubViewsLayout];
 }
 
-- (void)makeSubViewsLayout{
-    //添加约束
-}
 
 @end
